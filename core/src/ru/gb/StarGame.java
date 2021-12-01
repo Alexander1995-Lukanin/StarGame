23751544c62744eaf9d674ffbaf3d04f0130f11e
Alexander <kungfpanda@mail.ru>
2021-12-01 14:23:43 +0300

    Лекция 4

--- core/src/ru/gb/StarGame.java
+++ core/src/ru/gb/StarGame.java
@@ -1,19 +1,13 @@
 package ru.gb;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.utils.ScreenUtils;
 
 import ru.gb.screen.impl.MenuScreen;
 
 public class StarGame extends Game {
+
 	@Override
 	public void create() {
-		setScreen(new MenuScreen());
+		setScreen(new MenuScreen(this));
 	}
 }
